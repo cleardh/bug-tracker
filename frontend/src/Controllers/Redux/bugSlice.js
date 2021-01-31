284dd136f5d2d5011b59d5ae00ba32e75e763bab
Dongha Kang <dongha@irisbooth.com>
2021-01-31 18:41:58 -0500

    Removed unnecessary files & created redux slices and model

---
+++ frontend/src/Controllers/Redux/bugSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+    name: 'bug',
+    initialState: [],
+    reducers: {
+        getBugs: (state) => {
+
+        },
+        createBugs: (state, action) => {
+
+        },
+        updateBugs: (state, action) => {
+            
+        },
+        markComplete: (state, action) => {
+            
+        }
+    }
+})
---
+++ frontend/src/Controllers/bugController.js
@@ -0,0 +1,32 @@
+import bugModel from '../Models/BugModel'
+
+export const retrieveBugs = () => {
+    let data = [];
+    
+    data.push(new bugModel({
+        _id: 23456789,
+        name: 'Crash on load',
+        details: 'Crash after 3 seconds',
+        steps: 'Open app and it will crash',
+        version: 'V2.0',
+        priority: 1,
+        assigned: 'Dongha Kang',
+        creator: 'test',
+        time: '18:20'
+    }));
+    
+    data.push(new bugModel({
+        _id: 23456789,
+        name: 'Will not load',
+        details: 'Crash after 3 seconds',
+        steps: 'Open app and it will crash',
+        version: 'V2.0',
+        priority: 3,
+        assigned: 'Dongha Kang',
+        creator: 'test',
+        time: '18:34'
+    }));
+
+    let sorted = data.sort((a, b) => { return a.priority - b.priority; })
+    return sorted;
+}
