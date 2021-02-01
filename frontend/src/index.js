cf1fa1d668a35e65dce76041aa0f046b0d48448c
Dongha Kang <dongha@irisbooth.com>
2021-01-31 20:03:35 -0500

    Set up redux configuration to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import App from './App';
 
+// Reducers
+import authReducer from './Controllers/Redux/authSlice';
+import bugReducer from './Controllers/Redux/bugSlice';
+import userReducer from './Controllers/Redux/userSlice';
+
+// Redux configuration
+const reducer = combineReducers({
+  auth: authReducer,
+  bug: bugReducer,
+  user: userReducer
+});
+
+const store = configureStore({
+  reducer
+})
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
