050de7357af649f73544fbd983ac7aaa7d808b6a
Dongha Kang <dongha@irisbooth.com>
2021-02-09 00:30:27 -0500

    Added dashboard showing bugs categorized depending on priority

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from './Views/Pages/Login/login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Sidebar from './Views/Sidebar/sidebar';
 
+import DashboardPage from './Views/Pages/Dashboard/dashboard';
 import ViewBugPage from './Views/Pages/viewBugs';
 import CreateBug from './Views/Components/BugCreateEdit/bugForm';
 
@@ -15,6 +16,7 @@ function App() {
         <>
           <Sidebar />
           <Switch>
+            <Route exact path='/'><DashboardPage /></Route>
             <Route path='/viewbugs'><ViewBugPage /></Route>
             <Route path='/createbugs'>
               <div className='page-container'>
