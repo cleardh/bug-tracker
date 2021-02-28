80c3611b1f0177f5f127d79200da555465796fb2
Dongha Kang <dongha@irisbooth.com>
2021-02-28 15:31:24 -0500

    Show selected menu on sidebar

--- frontend/src/Views/Sidebar/sidebar.jsx
+++ frontend/src/Views/Sidebar/sidebar.jsx
@@ -13,13 +13,20 @@ export default () => {
 
     const { auth } = useSelector(state => state);
 
+    const setBackground = (e) => {
+        e.target.parentNode.parentNode.childNodes.forEach(node => {
+            node.removeAttribute('style');
+        });
+        e.target.parentNode.setAttribute('style', 'background: #f17105');
+    }
+
     return (
         <div className='sidebar'>
             <Link className='nav-link' to='/'><h1 className='brand'>Bug-Tracker</h1></Link>
             <ul>
-                <li><Link to='/' className='nav-link'>Dashboard</Link></li>
-                <li><Link to='/viewbugs' className='nav-link'>View Bugs</Link></li>
-                {auth.admin && <li><Link to='/createbugs' className='nav-link'>Create Bug</Link></li>}
+                <li><Link onClick={setBackground} to='/' className='nav-link'>Dashboard</Link></li>
+                <li><Link onClick={setBackground} to='/viewbugs' className='nav-link'>View Bugs</Link></li>
+                {auth.admin && <li><Link onClick={setBackground} to='/createbugs' className='nav-link'>Create Bug</Link></li>}
             </ul>
             <button className='nav-link logout' onClick={logOut}>Logout</button>
         </div>
